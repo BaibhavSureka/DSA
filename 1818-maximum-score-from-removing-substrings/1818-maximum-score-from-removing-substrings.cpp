cfc4db2b7d83e39db47521885b0fe6e6ae3e97ca
Baibhav Sureka <83425243+BaibhavSureka@users.noreply.github.com>
2025-07-23 23:59:59 +0530

    Time: 49 ms (57.6%), Space: 28.6 MB (51.82%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
+        if (x < y) {
+            swap(x, y);
+            for (char &ch : s) {
+                if (ch == 'a') ch = 'b';
+                else if (ch == 'b') ch = 'a';
+            }
+        }
         
+        int score = 0;
+        stack<char> st;
+
+        // First pass: remove "ab"
+        for (char ch : s) {
+            if (!st.empty() && st.top() == 'a' && ch == 'b') {
+                st.pop();
+                score += x;
+            } else {
+                st.push(ch);
+            }
+        }
+
+        // Second pass: remove "ba"
+        string remaining;
+        while (!st.empty()) {
+            remaining += st.top();
+            st.pop();
+        }
+        reverse(remaining.begin(), remaining.end());
+
+        for (char ch : remaining) {
+            if (!st.empty() && st.top() == 'b' && ch == 'a') {
+                st.pop();
+                score += y;
+            } else {
+                st.push(ch);
+            }
+        }
+
+        return score;
     }
 };
